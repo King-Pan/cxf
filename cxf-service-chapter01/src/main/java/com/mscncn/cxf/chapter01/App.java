3dd393967013ed648e07eb2d5faa59cf5cd5aa00
pwpw1218@gmail.com <psz1218psz1218>
2015-06-11 22:55:58 +0800

    初始化maven项目，首次提交

---
+++ cxf-service-chapter01/src/main/java/com/mscncn/cxf/chapter01/App.java
@@ -0,0 +1,13 @@
+package com.mscncn.cxf.chapter01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
